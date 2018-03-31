2e1f938563be3745b6867c59e7feed799202a998
Doritanh <charrier.antho99@gmail.com>
2018-03-31 18:41:32 +0200

    Routes automatiques

--- server/routes.js
+++ server/routes.js
@@ -1,24 +1,51 @@
 const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-    identification : {
-        index : function(req, res) {
-           fs.readFile('client/identification/index.html', (err, data) => {
-               if (err) throw err;
-               res.writeHead(200, {"Content-Type": "text/html"});
-               res.write(data);
-               res.end();
-           });
-       },
-       css : {
-           default : function(req, res) {
-               fs.readFile('client/identification/css/default.css', (err, data) => {
-                   if (err) throw err;
-                   res.writeHead(200, {"Content-Type": "text/css"});
-                   res.write(data);
-                   res.end();
-               });
-           }
-       }
-    }
+// Tableau de mimeType
+const  mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml'
+};
+
+module.exports = function(req, res) {
+    // filePath contient l'adresse désiré
+    let filePath = '.' + req.url;
+    // Si l'adresse est la racine, on redirige vers le defaut
+    if (filePath == './') filePath = './client/identification/index.html';
+    // On selectionne l'extension contenu dans le filePath
+    let extname = String(path.extname(filePath)).toLowerCase();
+    // On set le type de contenu grâce au tableau du mimeType
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    // On essaye de lire le fichier contenu à l'adresse filePath
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            // Code d'erreur ENOENT, page non trouvé, redirection à l'index
+            if(error.code == 'ENOENT') {
+                fs.readFile('./client/identification/index.html', function(error, content) {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else { // Si autre, on renvoie 500 à l'utilisateur
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                res.end();
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
 }
