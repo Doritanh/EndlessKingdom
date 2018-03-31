c6a3222d1e4b8f6c1867873ab4f3102981bd3d3b
Doritanh <charrier.antho99@gmail.com>
2018-03-31 18:41:45 +0200

    Simplification avec module routes

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 const http = require('http');
 const routes = require('./server/routes');
 
-let server = http.createServer(function(req, res) {
-    switch (req.url) {
-        case '/':
-            routes.identification.index(req, res);
-            break;
-        case '/client/identification/css/default.css':
-            routes.identification.css.default(req, res);
-            break;
-        default:
-            routes.identification.index(req, res);
-            break;
-    }
-});
+http.createServer(function(request, response) {
+    routes(request, response);
+}).listen(80);
 
-server.listen(80);
-
-console.log("Serveur actif")
+console.log("Serveur actif");
