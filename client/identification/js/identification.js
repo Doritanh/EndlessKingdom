eeb7fe7addcefdd987d53f8ef424ffb83338abc8
Isoware <charly.printanier@hotmail.fr>
2018-04-01 02:04:31 +0200

    Ajout d'animation sur la page d'identification

--- client/identification/js/identification.js
+++ client/identification/js/identification.js
@@ -1,23 +1,34 @@
 let clicked = "connexion";
 
+//Trigger du bouton Connexion
 document.querySelector("#btn_connexion").onclick = function() {
     if(clicked == "inscription") {
         document.querySelector("#btn_connexion").style.borderStyle = "inset";
         document.querySelector("#btn_inscription").style.borderStyle = "outset";
-        document.querySelector("#connexion").style.display = "inline";
-        document.querySelector("#inscription").style.display = "none";
+        document.querySelector("#fade").style.opacity = 0;
         document.querySelector("#fenetre").style.height = "280px";
+        setTimeout(function() {
+            document.querySelector("#inscription").style.display = "none";
+            document.querySelector("#connexion").style.display = "inline";
+            document.querySelector("#fade").style.opacity = 1;
+        }, 300);
         clicked = "connexion";
     }
 }
 
+//Triger du bouton Inscription
 document.querySelector("#btn_inscription").onclick = function() {
     if(clicked == "connexion") {
         document.querySelector("#btn_inscription").style.borderStyle = "inset";
         document.querySelector("#btn_connexion").style.borderStyle = "outset";
-        document.querySelector("#connexion").style.display = "none";
-        document.querySelector("#inscription").style.display = "inline";
+        document.querySelector("#fade").style.opacity = 0;
         document.querySelector("#fenetre").style.height = "410px";
+        setTimeout(function() {
+            document.querySelector("#connexion").style.display = "none";
+            document.querySelector("#inscription").style.display = "inline";
+            document.querySelector("#fade").style.opacity = 1;
+        }, 300);
         clicked = "inscription";
     }
 }
+
