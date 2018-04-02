535bdec5b89f048362344d2bd4c4b0a54c3cea64
Doritanh <charrier.antho99@gmail.com>
2018-04-02 15:48:09 +0200

    Modification sockets

--- server/sockets.js
+++ server/sockets.js
@@ -1,7 +1,7 @@
 module.exports = function(wss) {
     wss.on('connection', function connection(ws) {
-        ws.on('message', function incoming(message) {
-            console.log('received: %s', message);
+        ws.on('message', function incoming(objet) {
+            console.log(objet);
         });
         ws.send('something');
     });
