a904a79487ee9f44cc7d813a209c24ca63826324
Doritanh <charrier.antho99@gmail.com>
2018-04-02 15:47:40 +0200

    Debut socket

--- client/identification/js/identification.js
+++ client/identification/js/identification.js
@@ -1,42 +1,18 @@
-window.Identification = {};
-window.Identification.clicked = "connexion";
+Identification.socket = new WebSocket('ws://' + window.location.hostname + ':8080');
 
-window.Identification.btnConnexion = function() {
-    if(Identification.clicked == "inscription") {
-        document.querySelector("#btn_connexion").style.borderStyle = "inset";
-        document.querySelector("#btn_inscription").style.borderStyle = "outset";
-        document.querySelector("#fade").style.opacity = 0;
-        document.querySelector("#fenetre").style.height = "280px";
-        setTimeout(function() {
-            document.querySelector("#inscription").style.display = "none";
-            document.querySelector("#connexion").style.display = "inline";
-            document.querySelector("#fade").style.opacity = 1;
-        }, 400);
-        Identification.clicked = "connexion";
-    }
-};
-
-window.Identification.btnInscription = function() {
-    if(Identification.clicked == "connexion") {
-        document.querySelector("#btn_inscription").style.borderStyle = "inset";
-        document.querySelector("#btn_connexion").style.borderStyle = "outset";
-        document.querySelector("#fade").style.opacity = 0;
-        document.querySelector("#fenetre").style.height = "410px";
-        setTimeout(function() {
-            document.querySelector("#connexion").style.display = "none";
-            document.querySelector("#inscription").style.display = "inline";
-            document.querySelector("#fade").style.opacity = 1;
-        }, 400);
-        Identification.clicked = "inscription";
-    }
-};
-
-window.addEventListener('load', function() {
-    let btnConnexion = document.querySelector("#btn_connexion");
-    let btnInscription = document.querySelector("#btn_inscription");
-
-    //Trigger du bouton Connexion
-    btnConnexion.addEventListener('click', Identification.btnConnexion);
-    //Triger du bouton Inscription
-    btnInscription.addEventListener('click', Identification.btnInscription);
-});
+(function() {
+    let socket = Identification.socket;
+    Identification.events = function() {
+        // Quand une connexion est effectué
+        socket.addEventListener('open', function (e) {
+            socket.send({
+                'nom' : 'bwah',
+                'contenu' : 'rien'
+            });
+        });
+        // Quand un message est reçu du serveur
+        socket.addEventListener('message', function (event) {
+            console.log('Message from server ', event.data);
+        });
+    };
+})();
