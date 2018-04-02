23533baabe21158e4487d6f3725ad678240dfc13
Doritanh <charrier.antho99@gmail.com>
2018-04-02 15:20:43 +0200

    Ajout socket

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const http = require('http');
+const WebSocket = require('ws');
 const routes = require('./server/routes');
 
 http.createServer(function(request, response) {
     routes(request, response);
 }).listen(80);
+
+const wss = new WebSocket.Server({ port: 8080 });
+const socket = require('./server/socket')(wss);
 
 console.log("Serveur actif");
---
+++ server/sockets.js
@@ -0,0 +1,8 @@
+module.exports = function(wss) {
+    wss.on('connection', function connection(ws) {
+        ws.on('message', function incoming(message) {
+            console.log('received: %s', message);
+        });
+        ws.send('something');
+    });
+}
