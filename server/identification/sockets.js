411014bd330a12d9e3dd6ad5bad2a95837049f2a
Doritanh <charrier.antho99@gmail.com>
2018-04-06 15:53:38 +0200

    Modifications erreurs

--- server/identification/sockets.js
+++ server/identification/sockets.js
@@ -6,16 +6,19 @@ module.exports = function(wss) {
             let id = JSON.parse(o).id;
             let data = JSON.parse(o).values;
             if (id === 'connexionDemande') {
-                identification.connexion(data.pseudo, data.mdp).then(n => {
-                    ws.send(n);
+                identification.connexion(data.pseudo, data.mdp).then(number => {
+                    ws.send(number);
                 }).catch(function(error) {
-                    console.log(error);
+                    console.log("ERREUR : ")
+                    console.log(error)
+                    ws.send(0);
                 });
             } else if (id === 'inscriptionDemande') {
                 identification.inscription(data.pseudo, data.mail, data.mdp, data.mdpConfirm).then(n => {
                     ws.send(n);
                 }).catch(function(error) {
-                    console.log(error);
+                    console.log("ERREUR : ")
+                    console.log(error)
                 });
             }
         });
