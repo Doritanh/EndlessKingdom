99799c21e003c45a760a999e166d2162b3b2ba36
Doritanh <charrier.antho99@gmail.com>
2018-04-06 22:49:25 +0200

    Hash password

--- server/identification/password.js
+++ server/identification/password.js
@@ -1,9 +1,23 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 module.exports = {
-    check : async function(mdpA, mdpB) {
-        if (mdpA === mdpB) return true;
-        return false;
+    check : async function(mdp, mdpHash) {
+        let promise = new Promise(function(resolve, reject) {
+            bcrypt.compare(mdp, mdpHash, function(err, res) {
+                if (err) return reject();
+                return resolve(res);
+            });
+        });
+        return promise;
     },
     hash : async function(mdp) {
-        return mdp;
+        let promise = new Promise(function(resolve, reject) {
+            bcrypt.hash(mdp, saltRounds, function(err, hash) {
+                if (err) return reject();
+                return resolve(hash);
+            });
+        });
+        return promise;
     }
 }
