a836be4a0be30966b278d74202044177f84238cf
Doritanh <charrier.antho99@gmail.com>
2018-04-06 15:53:26 +0200

    Commentaires + pas d'id get false

--- server/identification/identification.js
+++ server/identification/identification.js
@@ -4,18 +4,28 @@ const password = require('./password');
 module.exports = {
     /**
      *  Connexion d'un utilisateurs
-     *  @param {string} pseudo - Le pseudo de l'utilisateur
+     *  @param {string} pseudo - Pseudo de l'utilisateur
      *  @param {string} mdp - Le mot de passe non crypté de l'utilisateur
+     *  @return {number} - Codes de reussite ou d'erreurs
      */
     connexion : async function(pseudo, mdp) {
         let id = await requetes.getIDFromPseudo(pseudo);
-        if (typeof(id) === 'undefined' || id === null) return 2;
+        if (!id) return 2;
         let mdpID = await requetes.getMdp(id);
         let checkMdp = await password.checkPass(mdp, mdpID);
         if (!checkMdp) return 3;
         return 1;
     },
-    inscription : async function(pseudo, mail, mdp, mdp Confirm) {
+    /**
+     *  Inscription d'un utilisateur
+     *  @param {string} pseudo - Pseudo de l'utilisateur
+     *  @param {string} mail - Mail de l'utilisateur
+     *  @param {string} mdp - Mot de passe de l'utilisateur
+     *  @param {string} mdpConfirm - Mot de passe de confirmation de l'utilisateur
+     *  @return {number} - Codes de reussite ou d'erreurs
+     */
+    inscription : async function(pseudo, mail, mdp, mdpConfirm) {
+        if (mdp !== mdpConfirm) return 2;
         /*if (checkBugInscription(data)) return 2;
         if (await utilisateurs.checkPseudoAlreadyExist(data.pseudo)) return 3;
         if (await utilisateurs.checkMailAlreadyExist(data.adresse)) return 4;
