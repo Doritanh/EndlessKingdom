2454664c25c70ebbdb21146ad3818ca16dd72e20
Doritanh <charrier.antho99@gmail.com>
2018-04-07 18:23:44 +0200

    Split verification

--- client/identification/js/main.js
+++ client/identification/js/main.js
@@ -64,31 +64,36 @@ window.EndlessKingdom.identification.verification = {};
         else
             document.querySelector("#adresse").style.backgroundColor = "white";
     };
-    var verifConfMdp = function() {
-        if(document.querySelector("#confMdp").value != document.querySelector("#mdp").value)
-            document.querySelector("#confMdp").style.backgroundColor = "#FE5353";
-        else
-            document.querySelector("#confMdp").style.backgroundColor = "white";
-    };
     var verifMdp = function() {
-        //Verifie si il y a au moins une majuscule, une minuscule, un chiffre et entre 6 et 15 caractères
-        let reg = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])^.{6,15}$');
+        let reg = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])^.{6,15}$'); //Verifie si il y a au moins une majuscule, une minuscule, un chiffre et entre 6 et 15 caractères
 
         if (!reg.test(document.querySelector("#mdp").value)) {
             document.querySelector("#mdp").style.backgroundColor = "#FE5353";
         } else {
             document.querySelector("#mdp").style.backgroundColor = "white";
         }
+        verifConfMdp();
+    };
+    var verifConfMdp = function() {
+        if(document.querySelector("#confMdp").value != document.querySelector("#mdp").value)
+            document.querySelector("#confMdp").style.backgroundColor = "#FE5353";
+        else
+            document.querySelector("#confMdp").style.backgroundColor = "white";
     };
     EndlessKingdom.identification.verification = function() {
         //Verification email
+        document.querySelector("#adresse").addEventListener('keydown', verifEmail);
         document.querySelector("#adresse").addEventListener('keyup', verifEmail);
+
         //Verification fiabilité mot de passe
         document.querySelector("#mdp").addEventListener('keyup', verifMdp);
+        document.querySelector("#mdp").addEventListener('keydown', verifMdp);
+
         //Verification concordance des mots de passe
         document.querySelector("#confMdp").addEventListener('blur', verifConfMdp);
+        document.querySelector("#confMdp").addEventListener('keydown', verifConfMdp);
+        document.querySelector("#confMdp").addEventListener('keyup', verifConfMdp);
         document.querySelector("#confMdp").addEventListener('focus', verifConfMdp);
-    };
 })();
 
 /*
