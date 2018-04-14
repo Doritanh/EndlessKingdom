70a791b1373d38cce86f2aaa9632b7d65d85e714
Doritanh <charrier.antho99@gmail.com>
2018-04-14 19:31:15 +0200

    Ajout clavier

--- client/jeu/noyau/main.js
+++ client/jeu/noyau/main.js
@@ -6,19 +6,28 @@
 "use strict";
 window.EndlessKingdom = {};
 
+// Events du jeu
+import {events} from '/client/jeu/noyau/events.js';
+// Classes du jeu
 import {Ecran} from '/client/jeu/noyau/classes/Ecran.js';
 import {Camera} from '/client/jeu/noyau/classes/Camera.js';
+import {Clavier} from '/client/jeu/noyau/classes/Clavier.js';
+import {Menu} from '/client/jeu/noyau/classes/Menu.js';
 
 // Main du jeu
 (function() {
-    var ecran;
-    var camera;
+    var ecran, clavier;
 
     let init = function() {
         ecran = new Ecran(document.querySelector("#ecran"));
         ecran.dimension(800, 600);
         ecran.chargement();
-        camera = new Camera(ecran);
+        clavier = new Clavier();
+        events.addClavierEvents(clavier);
+    };
+
+    let menu = function() {
+        let menu = new Menu();
     };
 
     let lancer = function() {
