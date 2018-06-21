ca975fc7e95e6e2c9acd5825f2ef128e4b999314
Isoware <charly.printanier@hotmail.fr>
2018-06-21 15:14:26 +0200

    Perso de coté

--- client/jeu/js/ecran/vueEcran.js
+++ client/jeu/js/ecran/vueEcran.js
@@ -10,17 +10,59 @@ export class VueEcran extends Vue {
     }
 }
 
+
 VueEcran.prototype.dessiner = async function() {
     this._canvas.clear();
     let dataImage = JSON.parse(sessionStorage.getItem('ressources'));
+    //Images Decors
     let solPierre = new Image();
     solPierre.src = "data:image/png;base64," + dataImage['SolPierre'];
+    //------
+
+    //Images Persos
     let barbareFace = new Image();
+    let barbareDroite = new Image();
+    let barbareHaut = new Image();
+    let barbareGauche = new Image();
     barbareFace.src = "data:image/png;base64," + dataImage['BarbareFace'];
+    barbareDroite.src = "data:image/png;base64," + dataImage['BarbareDroite'];
+    barbareHaut.src = "data:image/png;base64," + dataImage['BarbareHaut'];
+    barbareGauche.src = "data:image/png;base64," + dataImage['BarbareGauche'];
+    //--------
+
     for(let i = 0; i < this._modele._salleAffiche._taille.x; i++) {
         for (let j = 0; j < this._modele._salleAffiche._taille.y; j++) {
             this._element.getContext('2d').drawImage(solPierre, i*32,j*32);
         }
     }
-    this._element.getContext('2d').drawImage(barbareFace, this._modele._playerPosition.x*32, this._modele._playerPosition.y*32);
+
+    //Animations
+    switch(this._modele._etatMouvement) {
+        case "idleBas" :
+            this._element.getContext('2d').drawImage(barbareFace, this._modele._playerPosition.x*32, this._modele._playerPosition.y*32);
+            break;
+        case "idleDroit" :
+            this._element.getContext('2d').drawImage(barbareDroite, this._modele._playerPosition.x*32, this._modele._playerPosition.y*32);
+            break;
+        case "idleHaut" :
+            this._element.getContext('2d').drawImage(barbareHaut, this._modele._playerPosition.x*32, this._modele._playerPosition.y*32);
+            break;
+        case "idleGauche" :
+            this._element.getContext('2d').drawImage(barbareGauche, this._modele._playerPosition.x*32, this._modele._playerPosition.y*32);
+            break;
+    }
+    //---------
+}
+
+VueEcran.prototype.animation = function() {
+    let dataImage = JSON.parse(sessionStorage.getItem('ressources'));
+    let barbareFace = new Image();
+    barbareFace.src = "data:image/png;base64," + dataImage['BarbareFace'];
+   // switch(this._modele._etatMouvement) {
+        //case "idleBas" :
+        console.log("bwah")
+            this._element.getContext('2d').drawImage(barbareFace, this._modele._playerPosition.x*32, this._modele._playerPosition.y*32);
+            //break;
+    //}
+    
 }
--- client/jeu/js/ressource.js
+++ client/jeu/js/ressource.js
@@ -6,16 +6,30 @@ export class Ressource {
 }
 
 Ressource.prototype.load = function(callback) {
+    //Ressources Decors
     let solPierre = new Image();
+    //----
+    //Ressources Persos
+    let barbareDroite = new Image();
+    let barbareHaut = new Image();
+    let barbareGauche = new Image();
     let barbareFace = new Image();
+    //----
     barbareFace.addEventListener('load', function() {
         let data = {};
         data['SolPierre'] = getBase64Image(solPierre);
         data['BarbareFace'] = getBase64Image(barbareFace);
+        data['BarbareDroite'] = getBase64Image(barbareDroite);
+        data['BarbareHaut'] = getBase64Image(barbareHaut);
+        data['BarbareGauche'] = getBase64Image(barbareGauche);
         callback(data);
     }, false);
     solPierre.src = '' + this._location + 'SolPierre.png';
+    barbareDroite.src = '' + this._location + 'BarbareDroite.png';
+    barbareHaut.src = '' + this._location + 'BarbareDos.png';
+    barbareGauche.src = '' + this._location + 'BarbareGauche.png'
     barbareFace.src = '' + this._location + 'BarbareFace.png';
+    
 }
 
 let getBase64Image = function(img) {
