a04d5a73b6ff7423eb9a93bbb05e83d72c78cc04
doritanh <charrier.antho99@gmail.com>
2018-06-25 08:59:14 +0200

    test

--- server/jeu/donjon.js
+++ server/jeu/donjon.js
@@ -257,15 +257,24 @@ let salles = function(matrice) {
 }
 
 let spawn = function(maxSalles, matrice) {
+    let spawn = generationNombre(0, maxSalles-1);
+    let compteur = 0;
+    let x = 0, y = 0;
     for (let i = 0; i < matrice.length; i++) {
         for (let j = 0; j < matrice[i].length; j++) {
             if (matrice[i][j] === 1) {
-                    return {
-                        'x' : i,
-                        'y' : j
-                    }
+                if (compteur === spawn) {
+                    x = i;
+                    y = j;
+                    break;
+                }
+                compteur++;
             }
         }
+    }
+    return {
+        'x' : x,
+        'y' : y
     }
 }
 let fin = function(maxSalles, matrice, spawn) {
@@ -274,6 +283,7 @@ let fin = function(maxSalles, matrice, spawn) {
     let x = 0, y = 0;
     for (let i = 0; i < matrice.length; i++) {
         for (let j = 0; j < matrice[i].length; j++) {
+            if (i != spawn.x || j != spawn.y) {
                 if (matrice[i][j] === 1) {
                     if (compteur === fin) {
                         x = i;
@@ -282,6 +292,7 @@ let fin = function(maxSalles, matrice, spawn) {
                     }
                     compteur++;
                 }
+            }
         }
     }
     return {
